a97e86c73e66f1efdac64505629700e200880213
puneet-kaushikji <puneetkaushik177@gmail.com>
2024-11-19 15:54:11 +0530

    first commit

---
+++ chatapp-frontend-main/src/constants/color.js
@@ -0,0 +1,10 @@
+export const orange = "#5e35b1";
+export const orangeLight = "#9575cd";
+
+export const grayColor = "rgba(247,247,247,1)";
+export const lightBlue = "#2694ab";
+export const matBlack = "#1c1c1c";
+export const bgGradient = "linear-gradient(225deg, hsla(261, 46%, 84%, 1) 0%, hsla(261, 46%, 74%, 1) 100%)";
+
+export const purple = "rgba(75,12,192,1)";
+export const purpleLight = "rgba(75,12,192,0.2)";
---
+++ chatapp-server-main/constants/config.js
@@ -0,0 +1,9 @@
+const corsOptions = {
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+const CHATTU_TOKEN = "chattu-token";
+
+export { corsOptions, CHATTU_TOKEN };
